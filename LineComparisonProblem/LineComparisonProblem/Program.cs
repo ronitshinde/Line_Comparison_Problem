a6bf7225b65b8401e446f4e08bb8714ce2a09d96
ronitshinde <rony.shinde753@gmail.com>
2023-04-24 12:14:07 +0530

    [Ronit] Add : UC3-Compared two lines to check equality greater and lesser one over the other

--- LineComparisonProblem/LineComparisonProblem/Program.cs
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -10,16 +10,18 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
-            LineComparison lineOne = new LineComparison(15, 14, 17, 8);
+            LineComparison lineOne = new LineComparison(5, 1, 7, 4);
             double lineLengthOne = lineOne.CompareTwoLines();
             Console.WriteLine($"Length of the line one : {lineLengthOne}");
-            LineComparison lineTwo = new LineComparison(25, 3, 15, 21);
+            LineComparison lineTwo = new LineComparison(15, 14, 17, 8);
             double lineLengthTwo = lineTwo.CompareTwoLines();
             Console.WriteLine($"Length of the line two : {lineLengthTwo}");
-            if(lineLengthOne.Equals(lineLengthTwo))
+            if (lineLengthOne.CompareTo(lineLengthTwo) == 0)
                 Console.WriteLine("Both lines are equal");
+            else if (lineLengthOne.CompareTo(lineLengthTwo) > 0)
+                Console.WriteLine($"Lenght of line one : {lineLengthOne} is greater than lenght of line two : {lineLengthTwo}");
             else
-                Console.WriteLine("Both lines are unequal");
+                Console.WriteLine($"Lenght of line one : {lineLengthOne} is lesser than length of line two : {lineLengthTwo}");
             Console.ReadKey();
         }
     }
