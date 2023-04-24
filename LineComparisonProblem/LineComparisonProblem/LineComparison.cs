366b91fe407e22fd3f425e0ed9bd5f1d2081f52e
ronitshinde <rony.shinde753@gmail.com>
2023-04-24 10:38:49 +0530

    [Ronit] Add : UC1-Calculate length of line

---
+++ LineComparisonProblem/LineComparisonProblem/LineComparison.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LineComparison
+    {
+        int x_One, x_Three, y_One, y_Two;
+        //Constructor
+        public LineComparison(int x_One, int x_Two, int y_One, int y_Two)
+        {
+            this.x_One = x_One;
+            x_Three = x_Two;
+            this.y_One = y_One;
+            this.y_Two = y_Two;
+        }
+        public void CompareTwoLines()
+        {
+            double length = Math.Sqrt(Math.Pow((x_Three - x_One), 2) + Math.Pow((y_Two - y_One), 2));
+            Console.WriteLine($"Length of the line : {length}");
+        }
+    }
+}
---
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LineComparison compare = new LineComparison(15, 24, 17, 8);
+            compare.CompareTwoLines();
+            Console.ReadKey();
+        }
+    }
+}
