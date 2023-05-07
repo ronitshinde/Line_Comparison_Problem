125a19906581a4e1c80872ce085671c2025fe28f
ronitshinde <rony.shinde753@gmail.com>
2023-05-07 12:04:19 +0530

    [Ronit] Add : UC4-Refactored code using oops concepts

--- LineComparisonProblem/LineComparisonProblem/Program.cs
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -13,15 +13,21 @@ namespace LineComparisonProblem
             LineComparison lineOne = new LineComparison(5, 1, 7, 4);
             double lineLengthOne = lineOne.CompareTwoLines();
             Console.WriteLine($"Length of the line one : {lineLengthOne}");
-            LineComparison lineTwo = new LineComparison(15, 14, 17, 8);
+            LineComparison lineTwo = new LineComparison(15, 5, 17, 8);
             double lineLengthTwo = lineTwo.CompareTwoLines();
             Console.WriteLine($"Length of the line two : {lineLengthTwo}");
+            Console.WriteLine("==============Comparing Two Lines===============");
             if (lineLengthOne.CompareTo(lineLengthTwo) == 0)
                 Console.WriteLine("Both lines are equal");
             else if (lineLengthOne.CompareTo(lineLengthTwo) > 0)
                 Console.WriteLine($"Lenght of line one : {lineLengthOne} is greater than lenght of line two : {lineLengthTwo}");
             else
                 Console.WriteLine($"Lenght of line one : {lineLengthOne} is lesser than length of line two : {lineLengthTwo}");
+            Console.WriteLine("==========Check Equality Of Two Lines===========");
+            if (lineLengthOne.Equals(lineLengthTwo))
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Both lines are unequal");
             Console.ReadKey();
         }
     }
